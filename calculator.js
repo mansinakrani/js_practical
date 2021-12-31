2090c7eca720f4415da906230ce5e3f4801d7ce9
mansinakrani <mansinakrani006@gmail.com>
2021-12-31 14:14:54 +0530

    functions added for MC, MR, M+, M-, MS

--- calculator.js
+++ calculator.js
@@ -31,3 +31,58 @@ let Answer = () => {
     });
 
 }
+
+
+// function memory save
+let memorysave = () =>{
+    num = result.value; 
+    if(Number.isNaN(num))return; // checks nan if yes then returned else will considered as number 
+    memoryRegister.push(num); //pushes the elements in array
+    list.innerHTML='';
+    memoryRegister.forEach((element) => {
+        list.innerHTML += '<li>' + element + '</li>'; //prints element in memory block 
+    });
+}
+
+//function memory plus
+let memoryplus = () => {
+    num = result.value;
+    if(Number.isNaN(num)) return; // checks nan if yes then returned else will considered as number 
+    lastvalue = list.lastChild.innerHTML; // takes last element of list and stores in lastvalue
+    ans = parseInt(lastvalue) + parseInt(num); //adds the last item in memory and the number
+    memoryRegister.pop(lastvalue);//pops out the lastvalue in array
+    memoryRegister.push(ans);//pushes the elements in array
+    list.innerHTML='';
+    memoryRegister.forEach((element) => {
+        list.innerHTML += '<li>' + element + '</li>'; //prints element in memory block 
+    });
+}
+
+//function memory minus
+let memoryminus = () => {
+    num = result.value;
+    if(Number.isNaN(num)) return; // checks  nan if yes then returned else will considered as number
+    lastvalue = list.lastChild.innerHTML;  // takes last element of list and stores in lastvalue
+    ans = parseInt(lastvalue) - parseInt(num); //substracts the last item in memory and the number
+    memoryRegister.pop(lastvalue);//pops out the lastvalue in array
+    memoryRegister.push(ans);//pushes the elements in array
+    list.innerHTML='';
+    memoryRegister.forEach((element) => {
+        list.innerHTML += '<li>' + element + '</li>'; //prints element in memory block 
+    });
+}
+
+//function memory recall 
+let memoryrecall = () => {
+    result.value = list.lastChild.innerHTML; //prints last element in list on display
+}
+
+//function memory clear
+let memoryclear = () => {
+    list.innerHTML='';
+    memoryRegister.forEach((element)=>{
+    while(memoryRegister.length){
+        memoryRegister.pop();
+    }
+  })
+}
