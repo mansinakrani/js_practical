2a3db58eb0a63ad93f62deab8ddb74072d0bfc7d
mansinakrani <mansinakrani006@gmail.com>
2021-12-31 13:44:59 +0530

    function added for radian to degree

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
-function Myfunction() {
+let result = document.getElementById("inputtext");
 
+//taking input and showing the numbers 1-10 and operators such as pi , e , mod etc
+let calculate = (number) => {
+    result.value += number;
+};
+
+//function radian to degree
+let deg = () => {
+    rad = result.value;
+    degr = (rad*180)/Math.PI;
+    answer.value = degr; 
 }
