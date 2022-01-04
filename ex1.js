794c017cb00dd1b4d795a0f36af5a28538d00e9b
mansinakrani <mansinakrani006@gmail.com>
2022-01-04 18:44:35 +0530

    remove ex1.js

--- ex1.js
+++
@@ -1,9 +0,0 @@
-function Myfunction() {
-    document.getElementById('fontSize').style.fontSize='20px';
-    document.getElementById('display').style.display='none';
-    console.log(5 + 6);
-}
-
-let x;
-x=1;
-document.getElementById("number").innerHTML=x;
